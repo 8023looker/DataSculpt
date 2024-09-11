ca7a0e8fc0f16dad28e266fac956774ea2fb2068
zhangshusen <zhangshusen@baichuan-inc.com>
2024-09-11 14:28:16 +0800

    isodata

---
+++ src/preprocessing/text_embedding.py
@@ -0,0 +1,142 @@
+# Phase 0: data preprocessing
+# doc truncate && embedding
+import os
+import json
+import ujson
+from pathlib import Path
+import zipfile
+from FlagEmbedding import BGEM3FlagModel
+import copy
+import ray
+import sys
+import numpy as np
+import logging
+import time
+
+formatter = logging.Formatter('[%(asctime)s] %(message)s', "%Y%m%d %H:%M:%S")
+def get_logger(name, log_file, level=logging.INFO):
+    handler = logging.FileHandler(log_file)
+    handler.setFormatter(formatter)
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+    return logger
+
+
+@ray.remote(num_cpus=2, num_gpus=1)
+class ModelBGEM3:
+    def __init__(self, model_path='BAAI/bge-m3'):
+        self.model = BGEM3FlagModel(model_path,  
+                                    use_fp16=True)
+    def get_paragraphs(self, file_content): # content → a single file
+        # context length
+        context_len = 16384 # enter your context length here, default to 16K
+        # store the result_dict, text
+        text_list, raw_text_list = [], []
+        for line in file_content: # 1st round
+            try:
+                line_dict = ujson.loads(line.replace("\n", "").replace('\\/', '/'))
+                
+                text = line_dict["text"] if "text" in line_dict else (line_dict["content"] if "content" in line_dict else "")
+                while text:
+                    raw_text_list.append(text[:context_len])
+                    text = text[context_len:]
+                
+            except ValueError as e: 
+                print(f"JSON parser error: {e}")
+                
+        # use bge_m3 model to embed
+        embedding_list = self.model.encode(raw_text_list, 
+                                           batch_size=3, 
+                                           max_length=8192,
+                                        )['dense_vecs'] # 2nd round
+        
+        # store the results
+        emb_index = 0
+        for line in file_content: # 3rd round         
+            try:
+                line_dict = ujson.loads(line.replace("\n", "").replace('\\/', '/'))              
+                # general
+                line_dict["source_id"] = line_dict.pop("docid") if "docid" in line_dict else ""
+                
+                # for Chinese
+                if "text" in line_dict:
+                    # modify the key in line_dict      
+                    line_dict["chunk"] = line_dict.pop("text") if "text" in line_dict else ""
+                    line_dict.pop("content") if "content" in line_dict else None # del "content" for Chinese       
+                # for English
+                elif "content" in line_dict:
+                    # modify the key in line_dict            
+                    line_dict["chunk"] = line_dict.pop("content") if "content" in line_dict else ""
+                # for "" (no content), abandon
+                
+                text = copy.deepcopy(line_dict["chunk"])
+                # truncate the chunk
+                while text:
+                    # embedding vector
+                    line_dict["vector_encoded"] = str(embedding_list[emb_index].tolist())       
+                    emb_index += 1
+                    # chunk
+                    line_dict["chunk"][:context_len] = text[:context_len]
+                    text_list.append(line_dict)
+                    text = text[context_len:]
+                                
+            except ValueError as e: 
+                print(f"JSON parser error: {e}")
+        
+        return text_list
+                
+    def handle_file(self, args):
+        i_file, o_file, log = args
+        status_dict = {'i_file': i_file, 'flag': True}
+        print(i_file)
+
+        # write in result .json data
+        with open(Path(i_file), 'r', encoding='utf-8', errors='ignore') as fin, open(Path(o_file), 'w', encoding = 'utf-8') as fout:
+            doc = fin.readlines()
+            rs_list = self.get_paragraphs(doc) # api file_content
+            json.dump(rs_list, fout, ensure_ascii=False)
+        
+        return status_dict 
+
+
+def main():
+    ray_log = get_logger('ray_emb', 'ray.log.emb')
+    ray_log.info('--- start new ray task ---')
+    ray.init(address='auto')
+    
+    input_folder = "/your/input/data_folder_path/" # your input data folder path ("xxx/DataSculpt/data_sample/input/")
+    output_root_folder = "/your/output/data_folder_path/" # your output data folder path ("xxx/DataSculpt/data_sample/embedding_rs/")
+    
+    args_list = []
+    for dirpath, dirnames, filenames in os.walk(input_folder):
+        for i, filename in enumerate(tqdm(filenames, total=len(filenames))):
+            file_path = os.path.join(dirpath, filename)
+            args_list.append((file_path, # input_file_path
+                              output_root_folder + filename, # output_file_path
+                              None # logs
+                            ))
+    
+    ray_log.info(f'total num of files: {len(args_list)}')
+
+    n_model = 104
+    models = [ ModelBGEM3.remote() for _ in range(n_model) ]
+    rs_ids = []
+
+    status_all = []
+    def process_incremental(status_all, status_dict): 
+        status_all.append(status_dict)
+        return status_all
+
+    for i in range(0, len(args_list), n_model):
+        rs_ids.extend([ m.handle_file.remote(args) for m, args in zip (models, args_list[i : i + n_model]) ])
+        
+    while len(rs_ids):
+        done_ids, rs_ids = ray.wait(rs_ids)
+        status_all = process_incremental(status_all, ray.get(done_ids[0]))
+        ray_log.info(status_all[-1])
+        ray_log.info(f'total {len(args_list)} tasks, {len(rs_ids)} tasks waiting, {len(status_all)} tasks done.')
+
+    
+if __name__ == "__main__":
+    main()
---
+++ src/semantic_clustering/node_num_decision.py
@@ -0,0 +1,116 @@
+# Phase 1. Semantic Clustering
+# 1.1. decide initial cluster number
+# depend on CPU ray cluster
+import faiss
+import numpy as np
+from tqdm import tqdm
+import ujson
+import ast
+import ray
+import os
+
+# FAISS
+dimension = 1024 # embedding dimension using bge-m3 is 1024
+
+# density list
+density_list = []
+
+# biuld FAISS index for each file
+@ray.remote
+def build_sample_index(args): # need args[0], args[1]
+    # Create an index  
+    index = faiss.IndexHNSWFlat(dimension, 32, faiss.METRIC_INNER_PRODUCT) # 64
+
+    input_file_path, faiss_output_path = args[0], args[1]
+    try:
+        embedding_list = [] # 2-dimension (for adding FAISS index)
+        with open(input_file_path, "r", encoding="utf-8", errors="ignore") as fin:
+            for idx, line in enumerate(fin):
+                try:
+                    line_dict = ujson.loads(line.replace("\n", "").replace('\\/', '/'))
+                    embedding_list.append(ast.literal_eval(line_dict["vector_encoded"]))
+                except ValueError as e:
+                    print(f"JSON parser error: {e}")
+        if len(embedding_list) > 0: 
+            vectors = np.array(embedding_list).astype('float32')
+            index.add(vectors)
+    
+    except OSError as e:  
+        print(f"An error occurred: {e}")
+    
+    # Save the index to disk
+    faiss.write_index(index, faiss_output_path)
+    
+@ray.remote
+def compute_density(args): # need args[0], args[1]
+    index = faiss.read_index(args[1]) # read FAISS index
+    
+    # get total line number
+    line_num = subprocess.run(['wc', '-l', args[0]], stdout=subprocess.PIPE)
+    line_count = int(line_num.stdout.split()[0])
+    print(f"total line number: {line_count}")
+    
+    part_density = 0
+    with open(args[0], "r", encoding="utf-8", errors="ignore") as fin:
+        for idx, line in enumerate(fin):
+            try:
+                line_dict = ujson.loads(line.replace("\n", "").replace('\\/', '/'))
+                search_vector = np.array([ast.literal_eval(line_dict["vector_encoded"])])
+                D, I = index.search(search_vector, line_count)
+                line_dict["cluster_distance"] = float(D[0][0])
+                
+                idx_density = np.mean(np.array([float(x) for x in D[0]])) # average distance for the current vector
+                
+                part_density = (idx_density + part_density * idx) / (idx + 1)
+            
+            except ValueError as e:
+                print(f"JSON 解析错误: {e}")
+                
+    density_list.append(part_density)
+
+# store status dictionary
+def process_incremental(status_all, status_dict):
+    status_all.append(status_dict)
+    return status_all
+    
+if __name__ == "__main__":
+    ray_log = get_logger("ray_faiss_index", "node_num_decision.log")
+    ray_log.info("--- start new ray task ---")
+    ray.init(address="auto")
+    
+    raw_data_folder = "/path/to/embedding_folder/" # path to the embedding folder (/xxx/DataSculpt/data_sample/embedding_rs/)
+    faiss_output_folder = "/path/to/part_faiss/" # path to the part_faiss folder (/xxx/DataSculpt/data_sample/faiss/part_faiss/)
+
+    args_list = []
+    for dirpath, dirnames, filenames in os.walk(raw_data_folder):
+        for i, filename in enumerate(tqdm(filenames, total=len(filenames))):
+            file_path = os.path.join(dirpath, filename)
+            args_list.append((file_path, faiss_output_folder + filename)) # [input_file_path, faiss_index_output_path]
+    
+    ray_log.info(f"total num of files: {len(args_list)}")
+    
+    status_all = []
+    # step1: build faiss index
+    rs_ids = [build_sample_index.remote(args) for args in args_list] # .options(memory=2.5 * 1024 * 1024 * 1024)
+    while len(rs_ids):
+        done_ids, rs_ids = ray.wait(rs_ids)
+        status_all = process_incremental(status_all, ray.get(done_ids[0]))
+        ray_log.info(status_all[-1])
+        ray_log.info(
+            f"total {len(args_list)} tasks, {len(rs_ids)} tasks waiting, {len(status_all)} tasks done."
+        )
+        
+    # step2: compute density
+    rs_ids = [compute_density.remote(args) for args in args_list] # .options(memory=2.5 * 1024 * 1024 * 1024)
+    while len(rs_ids):
+        done_ids, rs_ids = ray.wait(rs_ids)
+        status_all = process_incremental(status_all, ray.get(done_ids[0]))
+        ray_log.info(status_all[-1])
+        ray_log.info(
+            f"total {len(args_list)} tasks, {len(rs_ids)} tasks waiting, {len(status_all)} tasks done."
+        )
+
+    cluster_density = density_list.mean() # mean of density list
+    print(f"Semantic Density: {cluster_density}") # This result is all you need!
+    with open("semantic_density.txt", "w") as f:
+        f.write(str(cluster_density))
